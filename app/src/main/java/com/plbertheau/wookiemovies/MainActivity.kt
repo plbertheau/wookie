1c53cc323da6f7bc03195ac33815cb6feb62a3ab
plbertheau <plbertheau@gmail.com>
2024-07-25 09:18:16 +0200

    Wookie Project : navigation and dependencies

--- app/src/main/java/com/plbertheau/wookiemovies/MainActivity.kt
+++ app/src/main/java/com/plbertheau/wookiemovies/MainActivity.kt
@@ -4,25 +4,37 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.plbertheau.wookiemovies.ui.theme.TestTeleclinicTheme
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.plbertheau.wookiemovies.ui.screen.BottomNavItem
+import com.plbertheau.wookiemovies.ui.screen.BottomNavigationBar
+import com.plbertheau.wookiemovies.ui.theme.WookieMoviesTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TestTeleclinicTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+            WookieMoviesTheme {
+                val navController = rememberNavController()
+
+                Scaffold(bottomBar = { BottomNavigationBar(navController) }) { padding ->
+                    NavHost(
+                        modifier = Modifier.padding(padding),
+                        navController = navController,
+                        startDestination = BottomNavItem.Home.route,
+                    ) {
+                        composable(BottomNavItem.Home.route) { Greeting(name = "Android") }
+                        composable(BottomNavItem.Search.route) { /* Search Screen UI */ }
+                    }
                 }
             }
         }
@@ -40,7 +52,18 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    TestTeleclinicTheme {
-        Greeting("Android")
+    WookieMoviesTheme {
+        val navController = rememberNavController()
+
+        Scaffold(bottomBar = { BottomNavigationBar(navController) }) { padding ->
+            NavHost(
+                modifier = Modifier.padding(padding),
+                navController = navController,
+                startDestination = BottomNavItem.Home.route,
+            ) {
+                composable(BottomNavItem.Home.route) { Greeting(name = "Android") }
+                composable(BottomNavItem.Search.route) { /* Search Screen UI */ }
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/plbertheau/wookiemovies/ui/screen/BottomNavigations.kt
@@ -0,0 +1,46 @@
+package com.plbertheau.wookiemovies.ui.screen
+
+
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.navigation.NavController
+import androidx.navigation.compose.currentBackStackEntryAsState
+@Stable
+sealed class BottomNavItem(val route: String, val icon: ImageVector, val label: String) {
+    data object Home : BottomNavItem("home", Icons.Default.Home, "Home")
+    data object Search : BottomNavItem("search", Icons.Default.Search, "Search")
+    companion object {
+        fun values():List<BottomNavItem> = listOf(Home, Search)
+    }
+}
+
+@Composable
+fun BottomNavigationBar(navController: NavController) {
+    BottomNavigation {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route
+
+        BottomNavItem.values().forEach { item ->
+            BottomNavigationItem(
+                selected = currentRoute == item.route,
+                onClick = {
+                    navController.navigate(item.route) {
+                        popUpTo(navController.graph.startDestinationId)
+                        launchSingleTop = true
+                    }
+                },
+                icon = { Icon(item.icon, contentDescription = null) },
+                label = { Text(item.label) }
+            )
+        }
+    }
+}
